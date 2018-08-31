15305e22b06b33e8ca9ee7ccddfe9b7ca9542f50
SIEGE\S832491 <johnny.behague.umanis@axa.fr>
2018-08-31 14:05:14 +0200

    remove useless LoggingHelper

--- src/Handlers/LilaIntentHandler.ts
+++ src/Handlers/LilaIntentHandler.ts
@@ -1,4 +1,4 @@
-import { SpeakersHelper, HandlersHelper, MessagesHelper, LoggingHelper } from "../Helpers";
+import { SpeakersHelper, HandlersHelper, MessagesHelper } from "../Helpers";
 import { Response } from "ask-sdk-model";
 import { RequestHandler, HandlerInput } from "ask-sdk-core";
 import { ClassLogger } from 'rich-logger-decorator';
@@ -11,18 +11,16 @@ export default class LilaIntentHandler implements RequestHandler {
     handlersHelper: HandlersHelper;
     speakersHelper: SpeakersHelper;
     messagesHelper: MessagesHelper;
-    loggingHelper: LoggingHelper;
     canHandleRequest: (intentName: string,  handlersHelper: HandlersHelper, handlerInput: HandlerInput) => boolean;
     handleRequest: (messagesHelper: MessagesHelper, speakersHelper: SpeakersHelper, handlerInput: HandlerInput, messages?: string[], reprompts?: string[]) => Promise<Response>;
     
-    constructor(intentName: string, messages?: string[], reprompts?: string[], handlersHelper?: HandlersHelper, speakersHelper?: SpeakersHelper, messagesHelper?: MessagesHelper, loggingHelper?: LoggingHelper) {
+    constructor(intentName: string, messages?: string[], reprompts?: string[], handlersHelper?: HandlersHelper, speakersHelper?: SpeakersHelper, messagesHelper?: MessagesHelper) {
         this.intentName = intentName;
         this.messages = messages;
         this.reprompts = reprompts;
         this.handlersHelper = (handlersHelper) ? handlersHelper : new HandlersHelper();
         this.speakersHelper = (speakersHelper) ? speakersHelper : new SpeakersHelper();
         this.messagesHelper = (messagesHelper) ? messagesHelper : new MessagesHelper();
-        this.loggingHelper = (loggingHelper) ? loggingHelper : new LoggingHelper();
 
         this.canHandleRequest = (intentName: string,  handlersHelper: HandlersHelper, handlerInput: HandlerInput): boolean => {
             if(!intentName) { return true; }
@@ -35,10 +33,10 @@ export default class LilaIntentHandler implements RequestHandler {
 
 
     public canHandle(handlerInput: HandlerInput): boolean {
-        return this.loggingHelper.logException(this.canHandleRequest, true, this.intentName, this.handlersHelper, handlerInput);
+        return this.canHandleRequest(this.intentName, this.handlersHelper, handlerInput);
     };
 
     public async handle(handlerInput: HandlerInput): Promise<Response> {
-        return this.loggingHelper.logException(this.handleRequest, true, this.messagesHelper, this.speakersHelper, handlerInput, this.messages, this.reprompts);
+        return this.handleRequest(this.messagesHelper, this.speakersHelper, handlerInput, this.messages, this.reprompts);
     }
 }
